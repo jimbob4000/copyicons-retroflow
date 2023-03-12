7901dea0072eee1e4ac618106f20d2188b942126
jimbob4000 <71288909+jimbob4000@users.noreply.github.com>
2023-03-12 14:25:27 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
-# copyicons
+# copyicons - RetroFlow
+
+A fork of [copyicons](https://github.com/cy33hc/copyicons) by [cy33hc](https://github.com/cy33hc)
 
 This application is used for bulk copying of icon0.png/pic0.png from the ux0:app/GAME_ID/sce_sys to the ur0:appmeta/GAME_ID folder
 
 The psvita can only load 500 bubbles. If you have more then 500 apps/games installed. The vita will not create the icon0.png and pic0.png files in the ur0:appmeta folder for those games that don't have bubbles.
 
-This is used in conjuction with my Launcher app https://github.com/cy33hc/vita-launcher for getting icons to display for the games.
+**Modifications made to the original copyicons app**
+
+In addition to copying the icon0.png/pic0.png to the appmeta folders, the app also copies additional images into the RetroFlow ux0 data folder.
+
+* icon0 is copied to the RetroFlow ux0 data folder as a backup image for when the appmeta image isn't available.
+* In order of priority: pic0, bg, bg0 are copied to the RetroFlow ux0 data folder for use as game background images.
+
+There is no release for this, the eboot is bundled in with the [RetroFlow](https://github.com/jimbob4000/RetroFlow-Launcher) app.
 
 ## Features
 
 1. Decrypt the icon0.png/pic0.png and copy to ur0:appmeta.
 2. Like mentioned this app scans every game in the ux0:app folder and copies the icon0.png/pic0.png if it's not found in ur0:appmeta
+3. Copy the icon0.png/pic0.png/bg.png/bg0.png to the RetroFlow ux0 data folder.
+
 
 ## Controls
 There are no controls. Just run the app and wait for it to complete. Progress is shown on the screen.
 
 ## Credits
+
+cy33hc for the original [copyicons](https://github.com/cy33hc/copyicons) app.
+
 Thx to theFlow for Vitashell. I had copied the modules from Vitashell needed to pfs mount the game to decypt the content.
 https://github.com/TheOfficialFloW/VitaShell
--- src/main.c
+++ src/main.c
@@ -26,6 +26,12 @@ int main(int argc, char *argv[]) {
 	char meta_pic_path[512];
 	char meta_path[512];
 
+	// RetroFlow customisation
+		char retroflow_icon_path[512];
+		char retroflow_bg_path[512];
+		char bg_path[512];
+		char bg0_path[512];
+
 	do {
 		SceIoDirent dir;
 		memset(&dir, 0, sizeof(SceIoDirent));
@@ -39,6 +45,10 @@ int main(int argc, char *argv[]) {
 			sprintf(meta_icon_path, "ur0:appmeta/%s/icon0.png", dir.d_name);
 			sprintf(pic_path, "ux0:app/%s/sce_sys/pic0.png", dir.d_name);
 			sprintf(meta_pic_path, "ur0:appmeta/%s/pic0.png", dir.d_name);
+
+			// RetroFlow customisation
+				sprintf(bg_path, "ux0:app/%s/sce_sys/livearea/contents/bg.png", dir.d_name);
+				sprintf(bg0_path, "ux0:app/%s/sce_sys/livearea/contents/bg0.png", dir.d_name);
 
 			printf("Looking for icons to copy for %s\n", dir.d_name);
 			
@@ -82,6 +92,100 @@ int main(int argc, char *argv[]) {
 					printf("copied %s to %s\n", pic_path, meta_pic_path);
 				}
 			}
+
+			// RetroFlow customisation
+
+				// Copy icon and pic0
+					sprintf(retroflow_icon_path, "ux0:data/RetroFlow/ICONS/Sony - PlayStation Vita/%s.png", dir.d_name);
+					sprintf(retroflow_bg_path, "ux0:data/RetroFlow/BACKGROUNDS/Sony - PlayStation Vita/%s.png", dir.d_name);
+
+					// Recheck meta folders
+					int meta_icon_path_exists_after_copy = checkFileExist(meta_icon_path);
+					int meta_pic_path_exists_after_copy = checkFileExist(meta_pic_path);
+
+					// Check RetroFlow folders
+					int retroflow_icon_path_exists = checkFileExist(retroflow_icon_path);
+					int retroflow_bg_path_exists = checkFileExist(retroflow_bg_path);
+
+					// Copy icon if missing from RetroFlow folder
+					if (meta_icon_path_exists_after_copy && !retroflow_icon_path_exists)
+					{
+						copyFile(meta_icon_path, retroflow_icon_path, NULL);
+						printf("copied %s to %s\n", meta_icon_path, retroflow_icon_path);
+					}
+					// Copy pic if missing from RetroFlow folder
+					if (meta_pic_path_exists_after_copy && !retroflow_bg_path_exists)
+					{
+						copyFile(meta_pic_path, retroflow_bg_path, NULL);
+						printf("copied %s to %s\n", meta_pic_path, retroflow_bg_path);
+					}
+					// Copy bg if pic missing from RetroFlow folder
+					if (meta_pic_path_exists_after_copy && !retroflow_bg_path_exists)
+					{
+						copyFile(meta_pic_path, retroflow_bg_path, NULL);
+						printf("copied %s to %s\n", meta_pic_path, retroflow_bg_path);
+					}
+
+				// Copy bg
+
+					// Check ux0 for bg pic
+					int bg_path_exists = checkFileExist(bg_path);
+
+					// Check RetroFlow folder for image
+					int retroflow_bg_path_exists_after_copy = checkFileExist(retroflow_bg_path);
+
+					// Copy bg if there's no image in the RetroFlow folder
+					if (pfs_path_exists && ((bg_path_exists && !retroflow_bg_path_exists_after_copy) || (bg_path_exists && !retroflow_bg_path_exists_after_copy)))
+					{
+						if (pfsMount(game_path) == 0)
+						{
+							if (bg_path_exists && !retroflow_bg_path_exists_after_copy)
+							{
+								copyFile(bg_path, retroflow_bg_path, NULL);
+								printf("copied %s to %s\n", bg_path, retroflow_bg_path);
+							}
+							pfsUmount();
+						}
+					}
+					else if ((bg_path_exists && !retroflow_bg_path_exists_after_copy) || (bg_path_exists && !retroflow_bg_path_exists_after_copy))
+					{
+						if (bg_path_exists && !retroflow_bg_path_exists_after_copy)
+						{
+							copyFile(bg_path, retroflow_bg_path, NULL);
+							printf("copied %s to %s\n", bg_path, retroflow_bg_path);
+						}
+					}
+
+				// Copy bg0
+
+					// Check ux0 for bg0 pic
+					int bg0_path_exists = checkFileExist(bg0_path);
+
+					// Check RetroFlow folder for image
+					int retroflow_bg_path_exists_after_copy2 = checkFileExist(retroflow_bg_path);
+
+					// Copy bg0 if there's no image in the RetroFlow folder
+					if (pfs_path_exists && ((bg0_path_exists && !retroflow_bg_path_exists_after_copy2) || (bg0_path_exists && !retroflow_bg_path_exists_after_copy2)))
+					{
+						if (pfsMount(game_path) == 0)
+						{
+							if (bg0_path_exists && !retroflow_bg_path_exists_after_copy2)
+							{
+								copyFile(bg0_path, retroflow_bg_path, NULL);
+								printf("copied %s to %s\n", bg0_path, retroflow_bg_path);
+							}
+							pfsUmount();
+						}
+					}
+					else if ((bg0_path_exists && !retroflow_bg_path_exists_after_copy2) || (bg0_path_exists && !retroflow_bg_path_exists_after_copy2))
+					{
+						if (bg0_path_exists && !retroflow_bg_path_exists_after_copy2)
+						{
+							copyFile(bg0_path, retroflow_bg_path, NULL);
+							printf("copied %s to %s\n", bg0_path, retroflow_bg_path);
+						}
+					}
+
 		}
 	} while (res > 0);
 
